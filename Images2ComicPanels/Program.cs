473b3cc97d4ac848f5f304f357a5186b4caadb42
thieu <thieu@DESKTOP-MRII2KL>
2024-07-15 17:56:05 +0200

    Add project files.

---
+++ Images2ComicPanels/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Images2ComicPanels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: Collage [ImageFilesLocation]");
+                return;
+            }
+
+            string imageFilesDirectory = args[0];
+
+            if (!Directory.Exists(imageFilesDirectory))
+            {
+                Console.WriteLine(string.Format("Directory \"{0}\" does not exist.", imageFilesDirectory));
+                return;
+            }
+
+            List<string> filePaths = Util.GetImageFiles(imageFilesDirectory);
+
+            if (filePaths.Count == 0)
+            {
+                Console.WriteLine("No image-files found");
+                return;
+            }
+            else if (!Util.SquareRootIsInteger(filePaths.Count))
+            {
+                Console.WriteLine("number of image-files found not correct, square root of number of image-files should be an integer");
+                return;
+            }
+
+            List<Bitmap> bitmaps;
+            try
+            {
+                bitmaps = Util.ImageFiles2Bitmaps(filePaths);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                return;
+            }
+            int averageWidth = (int)bitmaps.Average(b => b.Width);
+            int averageHeight = (int)bitmaps.Average(b => b.Height);
+
+            Util.ResizeBitmaps(bitmaps, averageWidth, averageHeight);
+
+            Bitmap[,] bitmapsArray2D = Util.BitmapsList2Bitmaps2DimensionalArray(bitmaps);
+            Bitmap stitchedBitmap = Util.StitchBitmaps(bitmapsArray2D);
+
+            string outputFilePath = Path.Combine(imageFilesDirectory, "Collage.jpg");
+            stitchedBitmap.Save(outputFilePath, ImageFormat.Jpeg);
+            string message = string.Format("Images in {0} turned into collage {1}", imageFilesDirectory, outputFilePath);
+            Console.WriteLine(message);
+
+
+
+        }
+    }
+}
---
+++ Images2ComicPanels/Util.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Drawing;
+
+
+namespace Images2ComicPanels
+{
+    public class Util
+    {
+
+        public static List<string> GetImageFiles(string directory)
+        {
+
+            return Directory.GetFiles(directory, "*.*").Where(f => f.EndsWith(".jpg") || f.EndsWith(".jpeg") ||
+                              f.EndsWith(".png") || f.EndsWith(".gif") || f.EndsWith(".bmp")).ToList();
+
+        }
+
+        public static bool SquareRootIsInteger(int number)
+        {
+            int squareroot = (int)Math.Sqrt(number);
+
+            if (squareroot * squareroot == number)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static List<Bitmap> ImageFiles2Bitmaps(List<string> imageFiles)
+        {
+            List<Bitmap> bitmaps = new List<Bitmap>();
+            foreach (string imageFile in imageFiles)
+            {
+                Bitmap bitmap;
+                try
+                {
+                    bitmap = new Bitmap(imageFile);
+                }
+                catch
+                {
+                    string message = string.Format("{0} does not seem to be an image-file", imageFile);
+                    throw new Exception(message);
+                }
+                bitmaps.Add(bitmap);
+            }
+            return bitmaps;
+        }
+
+        public static void ResizeBitmaps(List<Bitmap> bitmaps, int resizedWidth, int resizedHeight)
+        {
+            for (int counter = 0; counter < bitmaps.Count; counter++)
+            {
+                Bitmap bitmap = bitmaps[counter];
+                Bitmap resizedBitmap = new Bitmap(bitmap, new Size(resizedWidth, resizedHeight));
+                bitmaps[counter] = resizedBitmap;
+            }
+        }
+
+        public static Bitmap[,] BitmapsList2Bitmaps2DimensionalArray(List<Bitmap> bitmaps)
+        {
+            int numberOfColumns = (int)Math.Sqrt(bitmaps.Count);
+            int numberOfRows = (int)Math.Sqrt(bitmaps.Count);
+            Bitmap[,] bitmapArray2D = new Bitmap[numberOfColumns, numberOfRows];
+
+            int column = 0;
+            int row = 0;
+            foreach (Bitmap bitmap in bitmaps)
+            {
+                bitmapArray2D[row, column] = bitmap;
+                column++;
+                if (column > numberOfColumns - 1)
+                {
+                    column = 0;
+                    row++;
+                }
+            }
+            return bitmapArray2D;
+        }
+
+        public static Bitmap StitchBitmaps(Bitmap[,] bitmaps)
+        {
+            //all bitmaps should have te same witdh and height
+            int columns = bitmaps.GetLength(0);
+            int rows = bitmaps.GetLength(1);
+            int bitmapWidth = bitmaps[0, 0].Width;
+            int bitmapHeight = bitmaps[0, 0].Height;
+            int totalWidth = bitmapWidth * columns;
+            int totalHeight = bitmapHeight * rows;
+
+            Bitmap stitchedBitmap = new Bitmap(totalWidth, totalHeight);
+
+            using (Graphics graafix = Graphics.FromImage(stitchedBitmap))
+            {
+                for (int column = 0; column < columns; column++)
+                {
+                    for (int row = 0; row < rows; row++)
+                    {
+                        int x = column * bitmapWidth;
+                        int y = row * bitmapHeight;
+                        Bitmap bitmap = bitmaps[column, row];
+                        graafix.DrawImage(bitmap, new Point(x, y));
+                    }
+                }
+            }
+            return stitchedBitmap;
+        }
+
+
+    }
+}
